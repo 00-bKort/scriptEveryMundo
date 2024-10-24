2b321806acc76fd412bc2f182e766b40e9cd291f
bryan cabrera <bryan.cabrera@lifemiles.com>
2024-10-24 11:41:50 -0600

    script

---
+++ every-mundo.js
@@ -0,0 +1,24 @@
+(function () {
+  if (window.__ADNETIFY_SCRIPT) {
+    console.log(
+      'PROS - Adnetify is already instantiated: ',
+      window.__ADNETIFY_SCRIPT
+    );
+    return;
+  }
+  var adnetify = document.createElement('script');
+  var src =
+    document.querySelectorAll('[data-container-id^="mm2-"]').length > 0
+      ? 'https://everymundo.github.io/registry-adnetify2/scripts/2.0.0-experimental-14/dev/adnetify.js'
+      : 'https://em-frontend-assets-dev.airtrfx.com/mm/1.1.10/adnetify.js';
+  adnetify.type = 'text/javascript';
+  adnetify.src = src;
+  adnetify.onload = function () {
+    console.info('PROS - Adnetify loaded and ready');
+  };
+  adnetify.onerror = function () {
+    console.info('PROS - Adnetify could not be loaded');
+  };
+  document.body.appendChild(adnetify);
+  window.__ADNETIFY_SCRIPT = src;
+})();
