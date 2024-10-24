e3ea878bfb20a68bc6c6e30c12cf4922eff37c08
Bryan Cortez <54410100+00-bKort@users.noreply.github.com>
2024-10-24 15:01:59 -0600

    Update every-mundo.js

--- every-mundo.js
+++ every-mundo.js
@@ -10,7 +10,7 @@
   var src =
     document.querySelectorAll('[data-container-id^="mm2-"]').length > 0
       ? 'https://everymundo.github.io/registry-adnetify2/scripts/2.0.0-experimental-14/dev/adnetify.js'
-      : 'https://00-bkort.github.io/scriptEveryMundo/every-mundo.js';
+      : 'https://00-bkort.github.io/scriptEveryMundo/script-adnetify.js';
   adnetify.type = 'text/javascript';
   adnetify.src = src;
   adnetify.onload = function () {
